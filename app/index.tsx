441106d06802ceb7f33cdba076312f099ff9b2b9
vaddadi-abhishek <abhiabhishek2801@gmail.com>
2025-04-21 13:16:58 +0530

    Google Logo added

--- app/index.tsx
+++ app/index.tsx
@@ -1,58 +1,68 @@
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import globalStyles from './src/styles/globalStyles'
-import { Link } from 'expo-router'
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import globalStyles from './src/styles/globalStyles';
+import { Link } from 'expo-router';
 
 export default function index() {
   return (
-      <View style = {globalStyles.container}>
-          <View style={styles.viewText}>
-              <Text style={styles.welcomeText}>Let's</Text>
-              <Text style={styles.welcomeText}>Go</Text>
-              <Text style={styles.paraText}>sailing across social media for saved posts.</Text>
-          </View>
-  
-          <View style={styles.btnView}>
-              <Link href='./src/(screens)/Home' asChild>
-                  <TouchableOpacity style={styles.btnStyles}>
-                      <Image
-                        //   source={require('../assets/images/right_arrow.png')}
-                          style={styles.btnImg}
-                      />
-                  </TouchableOpacity>
-              </Link>
-          </View>
+    <View style={globalStyles.container}>
+      <View style={styles.viewText}>
+        <Text style={styles.welcomeText}>Let's</Text>
+        <Text style={styles.welcomeText}>Go</Text>
+        <Text style={styles.paraText}>
+          sailing across social media for saved posts.
+        </Text>
       </View>
-    )
+
+      <View style={styles.btnWrapper}>
+        <Link href="./src/(screens)/Home" asChild>
+          <TouchableOpacity style={styles.btnStyles}>
+            <Image
+              source={{ uri: 'https://img.icons8.com/3d-fluency/375/google-logo.png' }}
+              style={styles.btnImg}
+            />
+            <Text style={styles.btnText}>Login with Google</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    viewText: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingStart: 44,
-    },
-    welcomeText: {
-        fontWeight: 800,
-        fontSize: 48,
-    },
-    paraText: {
-        width: 100,
-    },
-    btnView: {
-        marginStart: 100,
-        marginBottom: 50
-    },
-    btnStyles: {
-        width: 150,
-        paddingVertical: 14,
-        paddingHorizontal: 21,
-        borderRadius: 25,
-        alignItems: 'center',
-        backgroundColor: '#3573D1'
-    },
-    btnImg: {
-        width: 25,
-        height: 25
-    }
-})
+  viewText: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingStart: 44,
+  },
+  welcomeText: {
+    fontWeight: '800',
+    fontSize: 48,
+  },
+  paraText: {
+    width: 100,
+  },
+  btnWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  btnStyles: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+    paddingVertical: 14,
+    paddingHorizontal: 21,
+    borderRadius: 25,
+    backgroundColor: '#3573D1',
+  },
+  btnImg: {
+    width: 20,
+    height: 20,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '300',
+  },
+});
